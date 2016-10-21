8ec6cf67069b9af8845791a51b8433d6a218657c
godfrey22 <g710110437@gmail.com>
2016-10-21 21:27:53 +1100

    Search Controller

--- controller/searchController.php
+++ controller/searchController.php
@@ -6,5 +6,51 @@ class SearchController{
 	{
 		$this->_conn=$conn;
 	}
+
+	public function getFeature(){
+		include_once('model/property_feature.class.php');
+		$feature = new Property_Feature($this->_conn);
+
+		$query="SELECT * FROM feature";
+        $stmt = oci_parse($this->_conn, $query);
+        oci_execute($stmt); 
+        $result = oci_fetch($stmt);
+		if($result)
+        {
+        	$feature_result = array();
+        	while(oci_fetch($stmt))
+        	{
+        		$feature_result[oci_result($stmt, 1)]=oci_result($stmt, 2);
+        	}
+            return $feature_result;
+        }
+        else
+        {
+            return false;
+        }
+	}
+
+	public function getType(){
+		include_once('model/property_type.class.php');
+		$type = new Property_Type($this->_conn);
+
+		$query="SELECT * FROM property_type";
+        $stmt = oci_parse($this->_conn, $query);
+        oci_execute($stmt); 
+        $result = oci_fetch($stmt);
+		if($result)
+        {
+        	$type_result = array();
+        	while(oci_fetch($stmt))
+        	{
+        		$type_result[oci_result($stmt, 1)]=oci_result($stmt, 2);
+        	}
+            return $type_result;
+        }
+        else
+        {
+            return false;
+        }
+	}
 	
 }
